193bb02998d100273059d8c82b6b19734aa6bce8
Michael Wright <mdwrigh2@ncsu.edu>
2011-02-23 16:48:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SlashDevSlashComp::Application.config.secret_token = 'd619ff160eb79e82477cbb086d73f570885460c7dcabbf925f73bcd18534b6a392c6ab2d8608c94e5e65164fcddc30cc651a5d1f26e092eeaacb8ea9c4f68d3e'
