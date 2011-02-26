eef8900bf668b8b5f631dcae2f903e6188fe2249
Michael Wright <mdwrigh2@ncsu.edu>
2011-02-26 08:49:35 -0500

    Fixed problem where viewing problems when not logged in would cause an error

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,5 +1,8 @@
 class ProblemsController < ApplicationController
   def index
+    if !session[:user]
+      redirect_to(logins_path, :error => "Must be logged in to view problems")
+    end
     if session[:user].is_admin
       redirect_to(all_problems_path)
       return
